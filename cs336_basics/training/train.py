63bfd6ddf94f5f1118a1a3ea4c11c46090b0cf06
周俊康 <zjk@bytedance.com>
2026-02-15 20:16:38 +0800

    [CS336] fix: training loop

--- cs336_basics/training/train.py
+++ cs336_basics/training/train.py
@@ -1,11 +1,10 @@
 import os
 import tqdm
+import torch
 import typing
 import numpy as np
 import torch.nn as nn
 import torch.optim as optim
-from cs336_basics.training.dataloader import data_loader
-from cs336_basics.optimizer.Loss import Cross_Entropy
 def train(
     model: nn.Module,
     optim: optim.Optimizer,
@@ -13,7 +12,13 @@ def train(
     dataset_dir: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
     batch_size: int,
     context_length: int,
-    device: str
+    device: str,
+    data_loader: typing.Callable[
+        [np.array, int, int, str], typing.Tuple[torch.tensor, torch.tensor]
+    ],
+    Loss : typing.Callable[
+        [torch.tensor, torch.tensor], torch.tensor
+    ],
 ):
     dataset = np.memmap(dataset_dir, dtype="int32", mode="r")
     dataloader = data_loader(
@@ -27,6 +32,6 @@ def train(
         optim.zero_grad()
         inputs, target = dataloader()
         logits = model(inputs)
-        loss = Cross_Entropy(logits, target)
+        loss = Loss(logits, target)
         loss.backward()
         optim.step()
